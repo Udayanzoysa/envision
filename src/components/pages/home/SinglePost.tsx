4342f528991268d296c86b76a9859240e2d8345b
Udayanzoysa <unzoysa.un@gmail.com>
2025-01-12 09:21:43 +0530

    changes

--- src/components/pages/home/SinglePost.tsx
+++ src/components/pages/home/SinglePost.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from "react";
 import ArrowLeft from "../../common/logo/ArrowLeft";
 import { getPost } from "../../../lib/content";
